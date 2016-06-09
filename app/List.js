b7313c0e061e2ebaf16c965c3d28afe49cee26c8
cheshire-howe <joshzoff@gmail.com>
2016-06-09 18:04:12 -0400

    Add state and markdown

--- app/List.js
+++ app/List.js
@@ -5,10 +5,12 @@ import Card from './Card';
 class List extends Component {
   render() {
     let cards = this.props.cards.map((card) => {
-      return <Card id={card.id}
-             title={card.title}
-             description={card.description}
-             tasks={card.tasks} />
+      return <Card key={card.id}
+                   id={card.id}
+                   title={card.title}
+                   description={card.description}
+                   color={card.color}
+                   tasks={card.tasks} />
     });
 
     return (
